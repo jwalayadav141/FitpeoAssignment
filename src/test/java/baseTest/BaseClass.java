37c4f3f4513c6e93ae2a9a455593c80530ab0e1e
Jwala Yadav <jwalayadav141@gmail.com>
2024-11-24 16:59:09 +0530

    documentation

--- src/test/java/baseTest/BaseClass.java
+++ src/test/java/baseTest/BaseClass.java
@@ -12,8 +12,11 @@ import io.github.bonigarcia.wdm.WebDriverManager;
 
 public class BaseClass {
 	
+	// Created WebDriver obj instance
 	public WebDriver driver;
 	
+	// created setUp method in Beforeclass tag, which is used for setup all the pre-requisite for start the automation
+	// used chrome as browser and all the capability added here
 	@BeforeClass
 	public void setUp() {
 		WebDriverManager.chromedriver().browserVersion("77.0.3865.40").setup();
@@ -27,7 +30,7 @@ public class BaseClass {
 		options.addArguments("--disable-gpu");
 		driver = new ChromeDriver(options);
 		
-		// Launch URL
+		// Launch the specific URL
 		driver.get("https://www.fitpeo.com/");
 		
 		// Maximize the window
@@ -36,6 +39,8 @@ public class BaseClass {
 		// Set Implicit wait which will be applicable for whole class
 		driver.manage().timeouts().implicitlyWait(Duration.ofSeconds(10));
 	}
+	
+	// added teardown method for closing all the browser opened during session
 	@AfterClass
 	public void tearDown() {
 //		driver.quit();
